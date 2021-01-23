2155f9feb49aae1fc6b65a94c89e36f7fc2f3452
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-01-23 20:39:07 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!system/bin/sh
+#mulai
+echo Selamat datang silahkan install bahan terlebih dahulu
+sleep 4
+clear
+pkg update upgrade
+pkg install git
+pkg install bash
+pkg install nano
+pkg install busybox
+clear
+sleep 4
+pkg install python
+pip2 install requests 
+pip2 install mechanize
+pip2 install bs4
+clear
+sleep 4
