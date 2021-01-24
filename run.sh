bf71f2bd49b57e90b1dcfa99824a2171cf141137
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-01-24 12:57:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ pkg install git
 pkg install bash
 pkg install nano
 pkg install busybox
+pkg install toilet figlet
 clear
 sleep 4
 pkg install python
@@ -16,3 +17,15 @@ pip2 install mechanize
 pip2 install bs4
 clear
 sleep 4
+echo Silahkan Masukan Nama Anda Dibawah Ini
+read ne
+echo Selamat Datang $ne
+echo Mari Mulai
+sleep 2
+clear
+toilet -f standard -F gay "KUMPULAN PERINTAH TELNET"
+echo Ini Dia Menu-Menu Nya
+sleep 4
+echo 1.Perintah Telnet 1
+
+
