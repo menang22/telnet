cdf4f276197beaacdbeb6780996c4ecaa897a0fe
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-01-24 22:25:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,22 +11,22 @@ os.system('pkg install nano')
 os.system('pkg install busybox') 
 os.system('pkg install toilet figlet')
 os.system('clear') 
-sleep 4
-pkg install python
-pip2 install requests 
-pip2 install mechanize
-pip2 install bs4
-clear
-sleep 4
+os.system('sleep 4')
+os.system('pkg install python')
+os.system('pip2 install requests')
+os.system('pip2 install mechanize')
+os.system('pip2 install bs4')
+os.system('clear')
+os.system('sleep 4')
 echo Silahkan Masukan Nama Anda Dibawah Ini
 read -p "NAMA:" ne :
 echo Selamat Datang $ne
 echo Mari Mulai
-sleep 2
-clear
+os.system('sleep 2')
+os.system('clear')
 toilet -f standard -F gay "KUMPULAN PERINTAH TELNET"
 echo Ini Dia Menu-Menu Nya
-sleep 4
+os.system('sleep 4')
 echo 1.Perintah Telnet 1
 echo 2.Perintah Telnet 2
 echo 3.Perintah Telnet 3
